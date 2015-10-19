226f095bec2cf01fef900ba861c5438b988d28a7
edward <eduard-97@mail.ru>
2015-10-19 12:34:49 +0300

    dasdsadasdasda

---
+++ 5 urok masivi.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	const int N = 10;
+	int m[N] = { 0 };
+	for (int i = 0; i < N; i++) {
+		cin >> m[i];
+
+	}
+	for (int i = 0; i < N; i++) {
+
+		cout << m[i] << " ";
+	}
+return 0;
+} 
+
